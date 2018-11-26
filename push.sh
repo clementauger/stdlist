51637188cba51ff9ca12c5d9326d19f74f91d198
Clément Auger <clementauger888@gmail.com>
2018-11-26 19:14:33 +0100

    fix the push url

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git add -A
 git commit --message "Travis build: go ${TRAVIS_GO_VERSION}"
-git remote add origin https://clementauger:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-git push --set-upstream origin master
+git remote add origin2 https://clementauger:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+git push --set-upstream origin2 master
